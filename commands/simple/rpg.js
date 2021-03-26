24a15b7c48a61c6413517aae4b3076bbe08ad16c
Lukas Batema <lukasbatema@gmail.com>
2021-03-26 16:11:34 -0400

    ffff

--- commands/simple/rpg.js
+++ commands/simple/rpg.js
@@ -25,7 +25,7 @@ module.exports = class rpg extends Command {
           .setDescription(`
             This Command is a WIP, please check back later!
           `)
-        message.channel.send(Scramble3);
+        message.channel.send(rpgHelp);
     }
   }
 }
