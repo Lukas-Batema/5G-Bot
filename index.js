1cd51f386d4a1d7b4e67406399e43f1f3efb0b27
Lukas Batema <LukasBatema@gmail.com>
2021-03-29 14:23:02 -0400

    oopdtate

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { CommandoClient } = require("discord.js-commando"); //Refer to https://discord.js.org/#/docs/commando/master/general/welcome for help.
-const BotData = require("./botData.js");//Imports custom BotData information for the bot.
+const botData = require("./botData.js");//Imports custom BotData information for the bot.
 const discord = require("discord.js"); //Refer to https://discord.js.org/#/docs/main/stable/general/welcome for help.
 const token = require("./token.js"); //Imports the token key for the bot to launch.
 const StormDB = require("stormdb");
