933773f1d223d54fca51374cdafae65c2db6b7a5
Lukas Batema <lukasbatema@gmail.com>
2021-03-30 20:27:18 -0400

    fix

--- commands/simple/rpg.js
+++ commands/simple/rpg.js
@@ -30,7 +30,7 @@ module.exports = class rpg extends Command {
       message.channel.send(rpgHelp);
       console.log("OH NO! Someone used the command, \"5G rpg " + reason + "\". Please persistently annoy GameHogPlays#0119 on Discord of this error!")
     } else if (reason == "quickfight" || reason == "quickf" || reason == "qfight" || reason == "qf") {
-      var placeHolderDisplayName = new discord.GuildMember.fetch(new discord.User.name);
+      var placeHolderDisplayName = new discord.User(this.memberName);
       db.get("users").set(placeHolderDisplayName, 100)
       .save();
 
